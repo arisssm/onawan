268522400a41e10368616ec7d03e1ac8b241528a
arisssm <arisssmulianto@gmail.com>
2024-03-30 18:55:49 +0700

    setting layout and package

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,27 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+/** ====== Variabel untuk mengakses route ====== */
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
+/** =========================================== */
+
 
 var app = express();
 
-// view engine setup
+/** ==== Setup Mongoose */
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/onawan');
+
+/** ===== */
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
+/** ====== Mengatur format file untuk tampilan ======*/
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+/** =========================================== */
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,15 +32,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/** ====== Mengakses template admin ======*/
+app.use('/sb-admin-2',express.static(path.join(__dirname,'node_modules/startbootstrap-sb-admin-2')));
+app.use('/admin-lte',express.static(path.join(__dirname,'node_modules/admin-lte')));
+/** =========================================== */
+
+/** ====== Mengakses route atau url browser ======*/
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
+/** ============================================== */
 
-// catch 404 and forward to error handler
+/** ====== Menangkap dan meneruskan ke bagian error handler ======*/
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+/** ====== Menangani error  pada aplikasi (error handler) ======*/
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
