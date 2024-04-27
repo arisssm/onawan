7a6cc450719d602450ee491a7a5b51b64df47316
arisssm <arisssmulianto@gmail.com>
2024-04-27 10:46:39 +0700

    update dashboard#1

--- backend/controller/dashboardController.js
+++ backend/controller/dashboardController.js
@@ -1,9 +1,20 @@
-
 module.exports = {
-    index: (req, res) => {
-        res.locals.title = 'Onawan | Dashboard'
-        res.locals.onPage = 'dashboard'
-        res.render('pages/dashboard');
-        // res.render('pages/home');
+    index: async (req, res) => {
+        try {
+            const alertMsg = req.flash('alertMsg');
+            const alertStatus = req.flash('alertStatus');
+            const alert = {
+                message: alertMsg,
+                status: alertStatus
+            }
+            res.locals.title = 'Onawan | Dashboard';
+            res.locals.onPage = 'dashboard';
+            res.render('pages/dashboard', {alert});
+        } catch(error) {
+            console.log(error.message);
+            req.flash('alertMsg', error.message );
+            req.flash('alertStatus', 'danger');
+            res.redirect('/admin/destination');
+        }
     }
 }
