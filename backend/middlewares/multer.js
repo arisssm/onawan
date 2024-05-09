1c5d83ba72803a56e34816dde0faadcf9ad294af
arisssm <arisssmulianto@gmail.com>
2024-05-09 18:35:41 +0700

    update multer#1

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -3,13 +3,6 @@ const path = require('path');
 
 /** === Konfigurasi Penyimpanan untuk File === */
 
-// const storage = multer.diskStorage({
-//     destination:'public/images',
-//     filename: function(req, file, cb){
-//     cb(null, Date.now() + Math.round(Math.random() * 1E9) + path.extname(file.originalname));
-//     }
-// });
-
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         let uploadPath = '';
@@ -26,21 +19,6 @@ const storage = multer.diskStorage({
         cb(null, `${uniqueFile}${path.extname(file.originalname)}`);
     }
 });
-
-/** === Validasi Type File === */
-
-// function checkTypeFile(file, cb)
-// {
-//     const fileType = /jpg|png|jpeg|gif/;
-//     const extName = fileType.test(path.extname(file.originalname).toLowerCase());
-//     const mimeType = fileType.test(file.mimetype);
-//     if (extName == mimeType)
-//     {
-//         return cb(null, true);
-//     } else {
-//         cb('Invalid type file');
-//     }
-// }
 
 function checkTypeFile(file, cb) {
     const fileType = ['image/jpeg', 'image/png', 'image/gif'];
