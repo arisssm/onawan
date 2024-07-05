6b20e08a330ec93dc0913121db29f9b815c82044
arisssm <arisssmulianto@gmail.com>
2024-07-05 13:09:43 +0700

    update pages reservation

--- frontend/src/pages/infoPage.jsx
+++ frontend/src/pages/infoPage.jsx
@@ -1,15 +1,113 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Form, Card, Button } from "react-bootstrap";
 import NavbarComponent from "../components/NavbarComponent";
 import FooterComponent from "../components/FooterComponent";
+import { useNavigate, useLocation } from "react-router-dom";
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import { jwtDecode } from 'jwt-decode';
 
 const infoPage = () => {
-    const [jumlahPenumpang, setJumlahPenumpang] = useState(1); // State untuk melacak jumlah penumpang
+    const { state } = useLocation();
+    const navigate = useNavigate();
+    const { flights, searchDetails} = state;
+    // console.log(flights);
+    const [jumlahPenumpang, setJumlahPenumpang] = useState(1);
+    const [ access, setAccess] = useState(true);
+    const [ userInfo, setUserInfo] = useState({ fullname: '', phone: '', email: ''});
+    const [ userTitle, setUserTitle] = useState('');
+    const [passangersDetails, setPassangerDetails] = useState(Array(searchDetails.totalPassangers).fill({ title: '', fullname: '', passengerType: ''}));
+    const tanggal = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+    const tanggalBerangkat = new Date(flights.departureTime).toLocaleDateString('id-ID', tanggal);
+    const totalPayment = flights.price * searchDetails.totalPassangers;
+    const MySwal = withReactContent(Swal);
+    const showAlert = ( title, text, icon) => {
+        MySwal.fire({
+            title: title,
+            text: text,
+            icon: icon,
+            confirmButtonText: 'Ok'
+        });
+    }
 
-    // Fungsi untuk menambah jumlah penumpang
     const tambahPenumpang = () => {
-        setJumlahPenumpang(jumlahPenumpang + 1);
+        if (jumlahPenumpang < searchDetails.totalPassangers) {
+            setJumlahPenumpang(jumlahPenumpang + 1);
+        }
     };
+
+    const getUser = () => {
+        try {
+            const token = localStorage.getItem('token');
+            if(!token){
+                setAccess(false);
+                showAlert('Gagal', 'Maaf tidak boleh akses halaman ini!', 'error');
+                navigate('/masuk');
+            } else {
+                setAccess(true);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const infoUser = () => {
+        const userDecode = jwtDecode(localStorage.getItem('token'))
+        setUserInfo(userDecode);
+    }
+
+    const handlePassangersDetailsChange = (index, field, value ) => {
+        const newPassangersDetails = [...passangersDetails];
+        if(!newPassangersDetails[index]) {
+            newPassangersDetails[index] =  {title: '', fullname: '', passengerType: ''}
+
+        }
+        newPassangersDetails[index][field] = value;
+        setPassangerDetails(newPassangersDetails);
+    }
+
+    const postReservation = async (event) => {
+        event.preventDefault();
+        try {
+            const body = {
+                userTitle: userTitle,
+                userId: userInfo.id,
+                passengers: passangersDetails,
+                flightId: flights._id,
+                reservationDate: new Date(),
+                totalPayment: totalPayment
+            }
+            console.log(body);
+
+            const authConfig = {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+            }
+
+            const response = await axios.post('http://127.0.0.1:3000/api/post-reservation', body, authConfig);
+            console.log(response.data.reservation);
+            // showAlert('Sukses', 'reservasi kamu telah dibuat, silahkan lanjut untuk pembayaran', 'error');
+            // const reservationId = response.data.reservation._id;
+
+            // if(response.statusText == 'OK'){
+            //     navigate('/bayar/' + reservationId, { state: { reservation: response.data.reservation}})
+            // } else {
+            //     navigate('/info');
+            //     showAlert('Error', 'data reservasi tidak benar', 'error');
+            // }
+        } catch (error) {
+            console.error('Cek lagi kode disini', error);
+        }
+    }
+
+
+
+    useEffect(()=>{
+        getUser();
+        infoUser();
+    }, [])
 
     return (
         <>
@@ -25,85 +123,102 @@ const infoPage = () => {
                             <Col lg={8}>
                                 <Card className='detail-pemesan'>
                                     <Form>
-                                        {['radio'].map((type) => (
-                                            <div className="mb-3 radios">
+                                        <div className="mb-3 radios">
+                                            {['Tuan', 'Nyonya', 'Nona'].map((type, index) => (
                                                 <Form.Check
+                                                    key={index}
                                                     type="radio"
-                                                    id="radio1"
-                                                    name="pilihan"
-                                                    label="Tuan"
+                                                    id={`radio-${type}`}
+                                                    name="userTitle"
+                                                    label={type}
+                                                    value={type}
+                                                    inline
+                                                    onChange={(e) => setUserTitle(e.target.value)}
                                                 />
-                                                <Form.Check
-                                                    type="radio"
-                                                    id="radio1"
-                                                    name="pilihan"
-                                                    label="Nyonya"
-                                                />
-                                                <Form.Check
-                                                    type="radio"
-                                                    id="radio1"
-                                                    name="pilihan"
-                                                    label="Nona"
-                                                />
-                                            </div>
-                                        ))}
+                                            ))}
+                                        </div>
 
-                                        <Form.Label>Nama Lengkap</Form.Label>
-                                        <Form.Control type="text" placeholder="Masukkan Nama Lengkap" name="nama" required></Form.Control>
+                                        <Form.Label htmlFor="fullname">Nama Lengkap</Form.Label>
+                                        <Form.Control
+                                            id= "fullname" 
+                                            type="text" 
+                                            value={userInfo.fullname}
+                                            style={{backgroundColor: 'whitesmoke'}}
+                                            readOnly
+                                        ></Form.Control>
 
-                                        <Form.Label>Nomor Telepon</Form.Label>
-                                        <Form.Control type="text" placeholder="Masukkan Nomor Telepon" name="telp" required></Form.Control>
+                                        <Form.Label htmlFor="phone">Nomor Telepon</Form.Label>
+                                        <Form.Control 
+                                            id= "phone"
+                                            type="text" 
+                                            value={userInfo.phone}
+                                            style={{backgroundColor: 'whitesmoke'}}
+                                            readOnly
+                                        ></Form.Control>
 
-                                        <Form.Label>Email</Form.Label>
-                                        <Form.Control type="email" placeholder="Masukkan Email" name="email" required></Form.Control>
+                                        <Form.Label htmlFor="email">Email</Form.Label>
+                                        <Form.Control 
+                                            id="email"
+                                            type="email"
+                                            value={userInfo.email} 
+                                            style={{backgroundColor: 'whitesmoke'}}
+                                            readOnly
+                                        >
+                                        </Form.Control>
                                     </Form>
                                 </Card>
 
                                 <div className="detail-penumpang mt-4">
                                     <h5 className="mb-3">Detail Penumpang</h5>
                                     <Card>
-                                        <Form action="/bayar">
+                                        <Form>
                                             {[...Array(jumlahPenumpang)].map((_, index) => (
                                                 <Card key={index} className="mb-3">
                                                     <Card.Title>Penumpang {index + 1}</Card.Title>
                                                     <hr />
                                                     <h6>Informasi Penumpang</h6>
-                                                    {['radio'].map((type) => (
-                                                        <div className="mb-3 radios" key={type}>
-                                                            <Form.Check
-                                                                type="radio"
-                                                                id={`radio${index}-${type}`}
-                                                                name={`pilihan${index}`}
-                                                                label="Tuan"
-                                                            />
-                                                            <Form.Check
-                                                                type="radio"
-                                                                id={`radio${index}-${type}`}
-                                                                name={`pilihan${index}`}
-                                                                label="Nyonya"
-                                                            />
-                                                            <Form.Check
-                                                                type="radio"
-                                                                id={`radio${index}-${type}`}
-                                                                name={`pilihan${index}`}
-                                                                label="Nona"
-                                                            />
+                                                        <div className="mb-3 radios">
+                                                            {['Tuan', 'Nyonya', 'Nona'].map((type) => (
+                                                                <Form.Check
+                                                                    key={type}
+                                                                    type="radio"
+                                                                    id={`radio${index}-${type}`}
+                                                                    name= {`title${index}`}
+                                                                    label={type}
+                                                                    value={type}
+                                                                    inline
+                                                                    onChange={(e) => handlePassangersDetailsChange(index, 'title', e.target.value)}
+                                                                />
+                                                            ))}
                                                         </div>
-                                                    ))}
 
-                                                    <Form.Label>Jenis Tiket</Form.Label>
-                                                    <Form.Control id={`jenisTiket${index}`} as="select">
+                                                    <Form.Label htmlFor="ticket">Jenis Tiket</Form.Label>
+                                                    <Form.Control
+                                                        id="ticket" 
+                                                        as="select"
+                                                        onChange= {(e) => handlePassangersDetailsChange(index, 'passengerType', e.target.value)}
+                                                    >
                                                         <option value="pilih">-- Jenis Tiket --</option>
-                                                        <option value="dewasa">Dewasa</option>
-                                                        <option value="remaja">Remaja</option>
-                                                        <option value="anak">Anak-anak</option>
+                                                        <option value="Dewasa">Dewasa</option>
+                                                        <option value="Anak-anak">Anak-anak</option>
                                                     </Form.Control>
 
-                                                    <Form.Label>Nama Lengkap</Form.Label>
-                                                    <Form.Control type="text" placeholder="Masukkan Nama Lengkap" name={`nama${index}`} />
+                                                    <Form.Label htmlFor="fullnamePassangers">Nama Lengkap</Form.Label>
+                                                    <Form.Control
+                                                        id="fullnamePassangers" 
+                                                        type="text" 
+                                                        placeholder="Masukkan Nama Lengkap" 
+                                                        onChange= {(e) => handlePassangersDetailsChange(index, 'fullname', e.target.value)} 
+                                                    />
                                                 </Card>
                                             ))}
-                                            <Button variant="btn btn-success mt-3" onClick={tambahPenumpang}>Tambah Penumpang</Button>
+                                            <Button 
+                                                variant="btn btn-success mt-3" 
+                                                onClick={tambahPenumpang}
+                                                disabled={jumlahPenumpang >= searchDetails.totalPassangers}
+                                            >
+                                                Tambah Penumpang
+                                            </Button>
                                         </Form>
                                     </Card>
                                 </div>
@@ -113,31 +228,39 @@ const infoPage = () => {
                                     <Row>
                                         <Col lg={6}>
                                             <p>Dari</p>
-                                            <h5>PALEMBANG</h5>
+                                            <h5>{flights.departureAirportId.city}</h5>
                                         </Col>
                                         <Col lg={6}>
                                             <p>Ke</p>
-                                            <h5>JAKARTA</h5>
+                                            <h5>{flights.arrivalAirportId.city}</h5>
                                         </Col>
                                     </Row>
                                     <hr />
                                     <p>Pilihan Maskapai</p>
-                                    <img src="../src/assets/ic-lionair.png" alt="" />
+                                    <img src={`http://127.0.0.1:3000/images/${flights.icon}`} alt="" />
                                     <p>Keberangkatan</p>
-                                    <h5>Senin, 15 Januari 2024</h5>
+                                    <h5>{tanggalBerangkat}</h5>
                                     <p>Waktu Keberangkatan</p>
-                                    <h5>07.00</h5>
+                                    <h5>
+                                        {new Date(flights.departureTime).toLocaleTimeString ([], {hour: '2-digit', minute: '2-digit'})}
+                                    </h5>
                                     <hr />
                                     <Row className="mt-3">
                                         <Col lg={6}>
                                             <h5 className="text-secondary">Total Pembayaran</h5>
                                         </Col>
                                         <Col lg={6}>
-                                            <h5>IDR 500.000</h5>
+                                            <h5>IDR {totalPayment}</h5>
                                         </Col>
                                     </Row>
                                 </Card>
-                                <Button href="/bayar" variant="btn btn-utama mt-3 w-100">Selanjutnya</Button>
+                                <Button 
+                                    onClick={postReservation}
+                                    type="button"
+                                    variant="btn btn-utama mt-3 w-100"
+                                >
+                                    Selanjutnya
+                                </Button>
                                 <p className='text-secondary mt-3'>*Pastikan data sudah terisi dengan benar sebelum melanjutkan.</p>
                             </Col>
                         </Row>
