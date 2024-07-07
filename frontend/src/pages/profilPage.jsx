6fe00b5c1b18d1994cba405d990bb324847023e1
arisssm <arisssmulianto@gmail.com>
2024-07-07 14:50:38 +0700

    update profile

--- frontend/src/pages/profilPage.jsx
+++ frontend/src/pages/profilPage.jsx
@@ -1,23 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Container, Modal, Card, Row, Col, Button, Form, Nav } from "react-bootstrap";
 import FooterComponent from "../components/FooterComponent";
 import NavbarComponent from "../components/NavbarComponent";
 import maskapai from "../assets/ic-lionair.png";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import { jwtDecode } from 'jwt-decode';
 
 const ProfilPage = () => {
-    const [show, setShow] = useState(false);
-    const handleClose = () => {
-        // untuk menyimpan data jika setelah edit
-        setFormData({
-            nama: document.getElementsByName("nama")[0].value,
-            telpon: document.getElementsByName("telpon")[0].value,
-            email: document.getElementsByName("email")[0].value
+    const navigate = useNavigate();
+    const MySwal = withReactContent(Swal);
+    const showAlert = ( title, text, icon) => {
+        MySwal.fire({
+            title: title,
+            text: text,
+            icon: icon,
+            confirmButtonText: 'Ok'
         });
+    }
 
-        // Tutup modal
-        setShow(false);
-    };
+    const [show, setShow] = useState(false);
     const handleShow = () => setShow(true);
 
     const [showLogoutModal, setShowLogoutModal] = useState(false);
@@ -25,12 +29,92 @@ const ProfilPage = () => {
     const handleCloseLogoutModal = () => setShowLogoutModal(false);
     const handleShowLogoutModal = () => setShowLogoutModal(true);
 
+    const [ payments, setPayments] = useState([]);
+    const [ user, setUser] = useState({});
+    const [ id, setId] = useState('');
+
+    const [ login, setLogin] = useState(true);
+
     // data bawaan
     const [formData, setFormData] = useState({
-        nama: "Jhon Doe",
-        telpon: "08123239819328",
-        email: "jhon@gmail.com"
+        id: '',
+        fullname: '',
+        phone: '',
+        email: '',
     });
+
+    const getUser = async() => {
+        try {
+            const token = localStorage.getItem('token');
+            if (token) {
+                const userDecode = jwtDecode(token);
+                console.log(userDecode);
+                setId(userDecode.id);
+                
+                const time = Date.now() / 1000;
+                if (userDecode.exp < time) {
+                    localStorage.removeItem('token');
+                    setLogin(false);
+                } else {
+                    setLogin(true);
+                }
+
+                const authConfig = {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+
+                const response = await axios.post('http://127.0.0.1:3000/api/update-user', authConfig);
+                console.log(response.data.user);
+                const userData = response.data.user;
+                setUser(userData);
+                setFormData({
+                    id: userData._id || '',
+                    fullname: userData.fullname || '',
+                    email: userData.email || '',
+                    phone: userData.phone || '',
+                })
+            } else {
+                setLogin(false);
+                showAlert('Gagal', 'Silahkan login kembali', 'error');
+                navigate('/masuk');
+            }
+        } catch (error) {
+            console.error('Invalid Token', error);
+        }
+    }
+
+    const getPayment = async() => {
+        try{
+            const response = await axios.get('http://127.0.0.1:3000/api/payment');
+            console.log(response.data.payment);
+            setPayments(response.data.payment)
+        } catch(error){
+            console.error('Cek kode ini, ada kesalahan', error)
+        }
+    }
+
+    const getSelectedPayment = () => {
+        return payments.filter(payment => 
+            payment.reservationId.userId._id === id && 
+            payment.status.toLowerCase() === activeKey.toLowerCase()
+        );
+    }
+
+    useEffect(()=>{
+        getPayment();
+        getUser();
+    }, [])
+    
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        navigate('/')
+    }
+
+    const handleClose = () => {
+        setShow(false);
+    };
 
     const handleChange = (e) => {
         setFormData({
@@ -44,63 +128,67 @@ const ProfilPage = () => {
         setActiveKey(selectedKey);
     };
 
-    const navigate = useNavigate();
-    const handleLogout = () => {
-        localStorage.removeItem('token');
-        navigate('/')
-    }
-
     const renderContent = () => {
-        return (
-            
-            <Card className="riwayat">
-                <Row>
-                    <Col lg={4}>
-                        <p>Dari</p>
-                        <h5>PALEMBANG</h5>
-                    </Col>
-                    <Col lg={4}>
-                        <p>Ke</p>
-                        <h5>JAKARTA</h5>
-                    </Col>
-                    <Col lg={4}>
-                        <p>Maskapai</p>
-                        <img src={maskapai} alt="" />
-                    </Col>
-                </Row>
-                <hr />
-                <Row>
-                    <Col lg={6}>
-                        <p>Keberangkatan</p>
-                        <h5>Senin, 15 Januari 2024</h5>
-                    </Col>
-                    <Col lg={6}>
-                        <p>Total Pembayaran</p>
-                        <h5>IDR 500.000</h5>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col lg={6}>
-                        <p>Waktu Keberangkatan</p>
-                        <h5>07.00 WIB</h5>
-                    </Col>
-                    <Col lg={6}>
-                        <p>Status</p>
-                        {activeKey === 'pending' &&
-                            <div className="set d-flex">
-                                <h6 className="pending">Belum Bayar</h6>
-                                <Link to="/konfirmasi" className="ms-3 mb-3">Lanjut Bayar →</Link>
-                            </div>}
-                        {activeKey === 'lunas' &&
-                            <div className="set d-flex">
-                                <h6 className="lunas">Lunas</h6>
-                                <Link to="/cetak" className="ms-3 mb-3">Lihat E-Tiket →</Link>
-                            </div>}
-                        {activeKey === 'selesai' && <h6 className="selesai">Selesai</h6>}
-                    </Col>
-                </Row>
-            </Card>
-        );
+        const userPayment = getSelectedPayment();
+        console.log(userPayment);
+        if (!userPayment || userPayment.length === 0) return null;
+        return userPayment.map((payment) => {
+            const tanggal = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+            const tanggalKeberangkatan = new Date(payment.reservationId.flightId.departureTime).toLocaleDateString('id-ID', tanggal);
+            const waktuKeberangkatan = new Date(payment.reservationId.flightId.departureTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    
+            return (
+                <Col key={payment._id}>
+                    <Card className="riwayat my-3">
+                    <Row>
+                        <Col lg={4}>
+                            <p>Dari</p>
+                            <h5>{payment.reservationId.flightId.departureAirportId.city}</h5>
+                        </Col>
+                        <Col lg={4}>
+                            <p>Ke</p>
+                            <h5>{payment.reservationId.flightId.arrivalAirportId.city}</h5>
+                        </Col>
+                        <Col lg={4}>
+                            <p>Maskapai</p>
+                            <img src={`http://127.0.0.1:3000/images/${payment.reservationId.flightId.icon}`} alt="" />
+                        </Col>
+                    </Row>
+                    <hr />
+                    <Row>
+                        <Col lg={6}>
+                            <p>Keberangkatan</p>
+                            <h5>{tanggalKeberangkatan}</h5>
+                        </Col>
+                        <Col lg={6}>
+                            <p>Total Pembayaran</p>
+                            <h5>IDR {payment.reservationId.totalPayment.toLocaleString('id-ID')}</h5>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={6}>
+                            <p>Waktu Keberangkatan</p>
+                            <h5>{waktuKeberangkatan}</h5>
+                        </Col>
+                        <Col lg={6}>
+                            <p>Status</p>
+                            {activeKey === 'belum bayar' &&
+                                <div className="set d-flex">
+                                    <h6 className="pending">Belum Bayar</h6>
+                                    <Link to="/tentang-kami" className="ms-3 mb-3">Lanjut Bayar Hub.Admin →</Link>
+                                </div>}
+                            {activeKey === 'lunas' &&
+                                <div className="set d-flex">
+                                    <h6 className="lunas">Lunas</h6>
+                                    <Link to="/cetak" className="ms-3 mb-3">Lihat E-Tiket →</Link>
+                                </div>}
+                            {activeKey === 'selesai' && <h6 className="selesai">Selesai</h6>}
+                        </Col>
+                    </Row>
+                    </Card>
+                </Col>
+            );
+        });
     }
 
     return (
@@ -131,7 +219,7 @@ const ProfilPage = () => {
                         <h5>Riwayat Transaksi</h5>
                         <Nav variant="pills" activeKey={activeKey} onSelect={handleSelect}>
                             <Nav.Item>
-                                <Nav.Link eventKey="pending">Belum Bayar</Nav.Link>
+                                <Nav.Link eventKey="belum bayar">Belum Bayar</Nav.Link>
                             </Nav.Item>
                             <Nav.Item>
                                 <Nav.Link eventKey="lunas">Tiket Tersedia</Nav.Link>
